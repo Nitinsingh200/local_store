245445e8f3da9b69a1eaff1a1e0f4a0380e2eb25
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-03-10 12:17:54 +0530

    json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
+// import React from 'react'
+
+// const App = () => {
+//   // localStorage.clear()
+//   // sessionStorage.clear()
+//   // localStorage.setItem("user","nitin")
+//    const user = localStorage.getItem('user')
+//     console.log(user)
+
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+
+
+// object ko string ky formet my send karna 
+// import React from 'react'
+
+// const App = () => {
+//    const user={
+//      username:'nitin',
+//      age:18,
+//      city:'bopal'
+
+//    }
+//     localStorage.setItem('user',JSON.stringify(user))
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+// object form my get karna hty user ko 
+
+
 import React from 'react'
 
 const App = () => {
-  // localStorage.clear()
-  // sessionStorage.clear()
-  // localStorage.setItem("user","nitin")
-   const user = localStorage.getItem('user')
+   const user = JSON.parse(localStorage.getItem('user'))
     console.log(user)
-
+   
   return (
     <div>App</div>
   )
